d0218ec4660d32bad4de1b8514e82c966b6a674c
naman3305 <59595534+naman3305@users.noreply.github.com>
2020-10-09 02:18:13 +0530

    Add files via upload

---
+++ StepsInInsertionSort.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main()
+{
+    int n,i,j,k=0,z;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=1;i<n;i++)
+    {
+        k=a[i];
+        j=i-1;
+
+        while(j>=0 && a[j]>k)
+        {
+            a[j+1]=a[j];
+            j=j-1;
+        }
+        a[j+1]=k;
+        for(z=0;z<n;z++)
+        {
+            printf("%d ",a[z]);
+        }
+        printf("\n");
+    }
+
+
+}
